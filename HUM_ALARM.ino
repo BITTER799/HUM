4ec78c250e878f522c481ab965f83fc0c8174956
BITTER799 <544340479@qq.com>
2017-01-20 11:03:10 +0800

    Update HUM_ALARM.ino

--- HUM_ALARM.ino
+++ HUM_ALARM.ino
@@ -2,11 +2,9 @@
 #include <Wire.h>
 #include "I2Cdev.h"
 #include <SHT2x.h>
-#define buzzer_pin 6
-#define buzzer_fre 600
-#define hum 50
+#define hum 50          //定义预设湿度值为50%
 #include <U8glib.h>
-#define INTERVAL_LCD             20             //定义OLED刷新时间间隔  
+#define INTERVAL_LCD   20             //定义OLED刷新时间间隔  
 unsigned long lcd_time = millis();  
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);     //设置OLED型号  
 //-------字体设置，大、中、小
@@ -15,7 +13,7 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);     //设置OLED型号
 #define setFont_S u8g.setFont(u8g_font_osb21)
 #define setFont_SS u8g.setFont(u8g_font_fub25n)
   
-  int sensor_hum;
+  int sensor_hum; 
   int sensor_tem;
  
 void setup()
@@ -30,19 +28,17 @@ void loop()
 {
   read();
   if(sensor_hum<=hum)
-  {
-    tone(buzzer_pin,buzzer_fre);
-    digitalWrite(A0,HIGH);
-    delay(10000);
-    noTone(buzzer_pin);
-    digitalWrite(A0,LOW);
-  }
-  Serial.print("hum(%RH):");
+  {   
+    digitalWrite(A0,HIGH);  //接通工作电路，让加湿器开始工作
+    delay(10000);    
+    digitalWrite(A0,LOW);  //关闭工作电路
+  }                      
+  Serial.print("hum(%RH):");                
   Serial.println(SHT2x.GetHumidity());
   Serial.print("tem(C):");
   Serial.println(SHT2x.GetTemperature());
   delay(1000);
-  u8g.firstPage();
+  u8g.firstPage();     //从这开始下面十行是让OLED显示温湿度
     do {
         setFont_S;
         u8g.setPrintPos(4, 32);
@@ -53,7 +49,7 @@ void loop()
         u8g.print(sensor_tem);
     }while( u8g.nextPage() );
 }
-void read()
+void read()   //自定义一个read函数
 {
   sensor_hum = SHT2x.GetHumidity() ;
   sensor_tem = SHT2x.GetTemperature();
